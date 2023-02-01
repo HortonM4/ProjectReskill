ecd3132b4ecf2ecac9b7ade61a7926b42defbdac
Martin Horton <martinhorton7@gmail.com>
2023-02-01 18:00:55 +0000

    .SQL file updated with full code for database creation

--- Project-EXPENSES/LifeStyle.sql
+++ Project-EXPENSES/LifeStyle.sql
@@ -1,4 +1,18 @@
-use LifeStyle;
+CREATE SCHEMA LifeStyle;
 
-select * from Expenses;
+USE LifeStyle;
 
+CREATE TABLE `LifeStyle`.`Expenses` (
+    `ID` INT NOT NULL,
+    `Housing` DOUBLE NOT NULL,
+    `Household_Bills` DOUBLE NOT NULL,
+    `Leisure` DOUBLE NOT NULL,
+    `Transport` DOUBLE NOT NULL,
+    `Holidays` DOUBLE NOT NULL,
+    `Clothes` DOUBLE NOT NULL,
+    `Savings` DOUBLE NOT NULL,
+    `Area` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`ID`));
+
+
+INSERT INTO `LifeStyle`.`Expenses` (`ID`, `Housing`, `Household_Bills`, `Leisure`, `Transport`, `Holidays`, `Clothes`, `Savings`, `Area`) VALUES ('1', '583.00', '457.78', '306.52', '144.83', '142.50', '43.88', '180.00', 'UK');
