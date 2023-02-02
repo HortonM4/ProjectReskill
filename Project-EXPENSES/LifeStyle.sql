f7d4607b386686ccd835351986a5c98a4ab68e45
Martin Horton <martinhorton7@gmail.com>
2023-02-02 09:49:36 +0000

    .SQL file updated with full code for database creation

--- Project-EXPENSES/LifeStyle.sql
+++ Project-EXPENSES/LifeStyle.sql
@@ -3,4 +3,5 @@ CREATE SCHEMA LifeStyle;
 USE LifeStyle;
 
 
+
 INSERT INTO `LifeStyle`.`Expenses` (`ID`, `Housing`, `Household_Bills`, `Leisure`, `Transport`, `Holidays`, `Clothes`, `Savings`, `Area`) VALUES ('1', '583.00', '457.78', '306.52', '144.83', '142.50', '43.88', '180.00', 'UK');
